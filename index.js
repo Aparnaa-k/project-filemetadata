8d5ae3c5336d06023d00601460d17e2fa5af3955
Aparnaa-k <aparnaa.kumar@kalvium.community>
2024-02-23 17:37:15 +0530

    commit-3

--- index.js
+++ index.js
@@ -25,6 +25,14 @@ app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
   });
 });
 
+app.use(
+  cors({
+    origin: "https://project-filemetadata-plum.vercel.app",
+    optionsSuccessStatus: 200,
+  })
+);
+
+
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log("Your app is listening on port " + port);
